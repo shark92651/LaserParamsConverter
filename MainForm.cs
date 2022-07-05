d5ee5d2a3515b3911a3a80c7951ed5c2cd0efe99
David Christian <david@riflegear.com>
2022-07-05 17:52:52 -0500

    Added Combine library management feature. Fixed decimal conversion error possible in some EZCAD libraries.

--- MainForm.cs
+++ MainForm.cs
@@ -47,6 +47,12 @@ namespace LaserParamsConverter
 			set { cbFormat.SelectedIndex = (int)value; }
 		}
 
+		private LibraryType CombineFormat
+		{
+			get { return (LibraryType)cbCombineFormat.SelectedIndex; }
+			set { cbCombineFormat.SelectedIndex = (int)value; }
+		}
+
 		private LaserType Laser
 		{
 			get { return (LaserType)cbLaser.SelectedIndex; }
@@ -61,15 +67,15 @@ namespace LaserParamsConverter
 
 		private int InLens
 		{
-			get 
+			get
 			{
 				if (tbInLens.Text == "NA")
 					return 1;
 				else
-					return int.Parse(tbInLens.Text, NumberFormatInfo.InvariantInfo); 
+					return int.Parse(tbInLens.Text, NumberFormatInfo.InvariantInfo);
 			}
 
-			set 
+			set
 			{
 				if (value == 1)
 				{
@@ -133,6 +139,7 @@ namespace LaserParamsConverter
 			{
 				cbCheckForUpdates.Checked = Settings.ChkForUpdates;
 				cbFormat.SelectedIndex = Settings.IdxFormat;
+				cbCombineFormat.SelectedIndex = Settings.IdxFormat;
 
 				prevLaserIndex = Settings.IdxLaser;
 				cbLaser.SelectedIndex = Settings.IdxLaser;
@@ -148,7 +155,7 @@ namespace LaserParamsConverter
 		{
 			switch (laser)
 			{
-				case LaserType.CO2 :
+				case LaserType.CO2:
 					MaxPower = Settings.MaxPowerCO2;
 					InLens = 1;
 					OutLens = 1;
@@ -270,8 +277,8 @@ namespace LaserParamsConverter
 				if (powerNode == null || speedNode == null)
 					continue;
 
-				power = int.Parse(powerNode.Attributes[0].Value, numStyles, NumberFormatInfo.InvariantInfo);
-				speed = int.Parse(speedNode.Attributes[0].Value, numStyles, NumberFormatInfo.InvariantInfo);
+				power = LaserHelper.ParseToInt(powerNode.Attributes[0].Value, numStyles, NumberFormatInfo.InvariantInfo);
+				speed = LaserHelper.ParseToInt(speedNode.Attributes[0].Value, numStyles, NumberFormatInfo.InvariantInfo);
 
 				pName = HttpUtility.HtmlDecode(node.ParentNode.ParentNode.Attributes["name"].Value);
 
@@ -298,9 +305,20 @@ namespace LaserParamsConverter
 
 		private void btnSaveParams_Click(object sender, EventArgs e)
 		{
-			dlgSaveFile.FilterIndex = (int)outputLib.Format +1;
+			if (sender == btnSaveParams)
+				SaveLibrary(outputLib);
+			else
+				SaveLibrary(combinedLib);
+		}
 
-			switch (outputLib.Format)	
+		private void SaveLibrary(LaserLibrary lib)
+		{
+			if (lib == null)
+				return;
+
+			dlgSaveFile.FilterIndex = (int)lib.Format + 1;
+
+			switch (lib.Format)
 			{
 				case LibraryType.EZCAD2:
 					dlgSaveFile.DefaultExt = "lib";
@@ -322,14 +340,7 @@ namespace LaserParamsConverter
 			{
 				LibraryType saveType = (LibraryType)dlgSaveFile.FilterIndex - 1;
 
-				//if ((outputLib.Format == LibraryType.EZCAD2 && saveType == LibraryType.EZCAD3) ||
-				//		(outputLib.Format == LibraryType.EZCAD3 && saveType == LibraryType.EZCAD2))
-				//{
-				//	MessageBox.Show("Converting between EZCAD formats is not supported", "Information", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-				//	return;
-				//}
-
-				if (outputLib.Format == LibraryType.LightBurn && saveType < LibraryType.LightBurn)
+				if (lib.Format == LibraryType.LightBurn && saveType < LibraryType.LightBurn)
 				{
 					MessageBox.Show("Converting from LightBurn to EZCAD formats is not supported", "Information", MessageBoxButtons.OK, MessageBoxIcon.Hand);
 					return;
@@ -344,21 +355,21 @@ namespace LaserParamsConverter
 
 				if (save)
 				{
-					bool convertToLightBurn = (outputLib.Format < LibraryType.LightBurn && saveType == LibraryType.LightBurn);
-					bool convertEZFormat = (outputLib.Format == LibraryType.EZCAD2 && saveType == LibraryType.EZCAD3) ||
-																 (outputLib.Format == LibraryType.EZCAD3 && saveType == LibraryType.EZCAD2);
+					bool convertToLightBurn = (lib.Format < LibraryType.LightBurn && saveType == LibraryType.LightBurn);
+					bool convertEZFormat = (lib.Format == LibraryType.EZCAD2 && saveType == LibraryType.EZCAD3) ||
+																 (lib.Format == LibraryType.EZCAD3 && saveType == LibraryType.EZCAD2);
 					bool convertToCSV = (saveType == LibraryType.CSV);
 
 					if (convertToLightBurn)
-						outputLib.SaveAsLightBurn(dlgSaveFile.FileName);
+						lib.SaveAsLightBurn(dlgSaveFile.FileName);
 					else if (convertEZFormat)
-						outputLib.SaveEZCAD(saveType, dlgSaveFile.FileName);
+						lib.SaveEZCAD(saveType, dlgSaveFile.FileName);
 					else if (convertToCSV)
-						outputLib.SaveAsCSV(dlgSaveFile.FileName);
+						lib.SaveAsCSV(dlgSaveFile.FileName);
 					else
-						outputLib.Save(dlgSaveFile.FileName);
-					
-					MessageBox.Show("Parameters File Saved!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+						lib.Save(dlgSaveFile.FileName);
+
+					MessageBox.Show("Library File Saved!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
 				}
 			}
 		}
@@ -415,8 +426,8 @@ namespace LaserParamsConverter
 
 			if (lvInputPower.SelectedItems.Count > 0)
 			{
-				inPower = int.Parse(lvInputPower.SelectedItems[0].SubItems[1].Text, NumberFormatInfo.InvariantInfo);
-				inSpeed = int.Parse(lvInputPower.SelectedItems[0].SubItems[2].Text, NumberFormatInfo.InvariantInfo);
+				inPower = LaserHelper.ParseToInt(lvInputPower.SelectedItems[0].SubItems[1].Text, NumberFormatInfo.InvariantInfo);
+				inSpeed = LaserHelper.ParseToInt(lvInputPower.SelectedItems[0].SubItems[2].Text, NumberFormatInfo.InvariantInfo);
 			}
 
 			ConvertOneForm dlg = new ConvertOneForm(Laser, MaxPower, InLens, InWatts, inPower, inSpeed, OutLens, OutWatts);
@@ -430,5 +441,245 @@ namespace LaserParamsConverter
 			Process.Start(sInfo);
 		}
 
+
+		LaserLibrary combineLib1;
+		LaserLibrary combineLib2;
+		LaserLibrary combinedLib;
+
+		private void btnCombine1_Click(object sender, EventArgs e)
+		{
+			SetInputFileFilter((LibraryType)cbCombineFormat.SelectedIndex);
+
+			if (dlgInputFile.ShowDialog() == DialogResult.OK)
+			{
+				cbCombineFormat.Enabled = false;
+				if (sender == btnCombine1)
+				{
+					tbCombine1.Text = dlgInputFile.FileName;
+					combineLib1 = new LaserLibrary(CombineFormat);
+					combineLib1.LoadLibrary(dlgInputFile.FileName);
+					DisplayLibrary(combineLib1, tvCombine1);
+				}
+				else
+				{
+					tbCombine2.Text = dlgInputFile.FileName;
+					combineLib2 = new LaserLibrary(CombineFormat);
+					combineLib2.LoadLibrary(dlgInputFile.FileName);
+					DisplayLibrary(combineLib2, tvCombine2);
+				}
+			}
+		}
+
+
+		private void DisplayLibrary(LaserLibrary lib, TreeView tv)
+		{
+			string mName;
+			string desc;
+			string title;
+
+			tv.BeginUpdate();
+			tv.Nodes.Clear();
+
+			foreach (XmlNode node in lib.XmlDoc.SelectNodes(lib.MaterialPath))
+			{
+				mName = HttpUtility.HtmlDecode(node.Attributes["name"].Value);
+
+				TreeNode matTreeNode = tv.Nodes.Add(mName);
+				matTreeNode.Tag = node;
+
+				// Force EZ2 "LASERMODE" node to be checked
+				if (lib.Format == LibraryType.EZCAD2 && mName == "LASERMODE")
+					matTreeNode.Checked = true;
+
+				if (lib.Format == LibraryType.LightBurn)
+				{
+					foreach (XmlNode eNode in node.ChildNodes)
+					{
+						desc = HttpUtility.HtmlDecode(lib.GetAttributeValue(eNode, "Desc", ""));
+						title = HttpUtility.HtmlDecode(lib.GetAttributeValue(eNode, "NoThickTitle", lib.GetAttributeValue(eNode, "Thickness", "")));
+						if (title == "-1.0000")
+							title = "(no thickness)";
+
+						TreeNode entTreeNode = matTreeNode.Nodes.Add(String.Format("{0} {1}", title, desc));
+						entTreeNode.Tag = eNode;
+					}
+				}
+
+				tv.ExpandAll();
+				if (tv.Nodes.Count > 0)
+					tv.Nodes[0].EnsureVisible();
+
+				tv.EndUpdate();
+			}
+		}
+
+		private void btnCopyNodes1_Click(object sender, EventArgs e)
+		{
+			if (sender == btnCopyNodes1)
+				CopyCheckedNodes(tvCombine1);
+			else
+				CopyCheckedNodes(tvCombine2);
+		}
+
+		private void CopyCheckedNodes(TreeView tv)
+		{
+			if (combinedLib == null)
+				combinedLib = new LaserLibrary(CombineFormat);
+
+			foreach (TreeNode matTreeNode in tv.Nodes)
+			{
+				XmlNode parentNode;
+
+				if (matTreeNode.Checked || CheckedChildNodeCount(matTreeNode) > 0)
+				{
+					// Find the parent Material node
+					parentNode = combinedLib.XmlDoc.SelectSingleNode(string.Format("{0}[@name='{1}']", combinedLib.MaterialPath, matTreeNode.Text));
+
+					if (parentNode == null)
+					{
+						XmlNode rootNode = combinedLib.XmlDoc.SelectSingleNode(combinedLib.RootPath);
+
+						// Need deep copy for EZ2 or EZ3 to get all settings
+						bool deep = (CombineFormat != LibraryType.LightBurn);
+
+						parentNode = combinedLib.XmlDoc.ImportNode((matTreeNode.Tag as XmlNode), deep);
+						rootNode.AppendChild(parentNode);
+					}
+
+					foreach (TreeNode entTreeNode in matTreeNode.Nodes)
+					{
+						if (entTreeNode.Checked)
+						{
+							XmlNode childNode = combinedLib.XmlDoc.ImportNode((entTreeNode.Tag as XmlNode), true);
+							parentNode.AppendChild(childNode);
+						}
+					}
+				}
+
+			}
+						
+			DisplayLibrary(combinedLib, tvCombined);
+			btnSaveCombined.Enabled = tvCombined.Nodes.Count > 0;
+		}
+
+
+		private int CheckedChildNodeCount(TreeNode node)
+		{
+			int cnt = 0;
+			foreach (TreeNode n in node.Nodes)
+			{
+				if (n.Checked)
+					cnt++;
+			}
+			return cnt;
+		}
+
+
+		private TreeView GetContextTreeView(object sender)
+		{
+			ToolStripItem menuItem = sender as ToolStripItem;
+			if (menuItem != null)
+			{
+				ContextMenuStrip owner = menuItem.Owner as ContextMenuStrip;
+				if (owner != null)
+				{
+					return (TreeView)owner.SourceControl;
+				}
+			}
+			return null;
+		}
+
+		private void miExpandAll_Click(object sender, EventArgs e)
+		{
+			TreeView tv = GetContextTreeView(sender);
+			if (tv != null)
+				tv.ExpandAll();
+		}
+
+		private void miCollapseAll_Click(object sender, EventArgs e)
+		{
+			TreeView tv = GetContextTreeView(sender);
+			if (tv != null)
+				tv.CollapseAll();
+
+		}
+
+		bool checkingAll = false;
+
+		private void miSelectAll_Click(object sender, EventArgs e)
+		{
+			checkingAll = true;
+			try
+			{
+				TreeView tv = GetContextTreeView(sender);
+				if (tv != null)
+					CheckAllTree(tv, true);
+			}
+			finally
+			{
+				checkingAll = false;
+			}
+		}
+
+		private void miUnselectAll_Click(object sender, EventArgs e)
+		{
+			checkingAll = true;
+			try
+			{
+				TreeView tv = GetContextTreeView(sender);
+			if (tv != null)
+				CheckAllTree(tv, false);
+			}
+			finally
+			{
+				checkingAll = false;
+			}
+		}
+
+		private void CheckAllTree(TreeView tv, bool v)
+		{
+			foreach (TreeNode node in tv.Nodes)
+			{
+				node.Checked = v;
+				CheckAllTreeNodes(node, v);
+			}
+		}
+
+		private void CheckAllTreeNodes(TreeNode node, bool v)
+		{
+			foreach (TreeNode n in node.Nodes)
+			{
+				n.Checked = v;
+			}
+		}
+
+		private void tvCombine1_AfterCheck(object sender, TreeViewEventArgs e)
+		{
+			if (!checkingAll)
+				CheckAllTreeNodes(e.Node, e.Node.Checked);
+
+			if (CombineFormat == LibraryType.EZCAD2 && e.Node.Text == "LASERMODE" && e.Node.Checked == false)
+				e.Node.Checked = true;
+
+		}
+
+		private void btnCombineReset_Click(object sender, EventArgs e)
+		{
+			Reset();
+		}
+
+		private void Reset()
+		{
+			combineLib1 = null;
+			combineLib2 = null;
+			combinedLib = null;
+			tvCombine1.Nodes.Clear();
+			tvCombine2.Nodes.Clear();
+			tvCombined.Nodes.Clear();
+			tbCombine1.Text = "Select library file 1";
+			tbCombine2.Text = "Select library file 2";
+			cbCombineFormat.Enabled = true;
+			btnSaveCombined.Enabled = false;
+		}
 	}
 }
